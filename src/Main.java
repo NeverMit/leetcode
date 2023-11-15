f579bb8abedfa82090dcaa57d385a60cf58ddc6a
NeverMit <markovnikov96@mail.ru>
2023-11-15 14:46:00 +0500

    1920. Build Array from Permutation

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums=new int[]{1,2,1};
-        int[] ans=new int[nums.length*2];
-        ans=getConcatenation(nums);
-        for(int i=0;i< ans.length;i++)
-            System.out.print(ans+" ");
+        int []nums=new int[]{0,2,1,5,3,4};
+        int[] ans=new int[nums.length];
+        ans=buildArray(nums);
+        for(int i=0;i< nums.length;i++)
+            System.out.print(ans[i]+" ");
     }
-    public static int[] getConcatenation(int[] nums) {
-        int[] ans=new int[2* nums.length];
+    public static int[] buildArray(int[] nums) {
+        int[] ans=new int[nums.length];
         for(int i=0;i< nums.length;i++){
-            ans[i]=nums[i];
-            ans[i+ nums.length]=nums[i];
+            ans[i]=nums[nums[i]];
         }
         return ans;
     }
